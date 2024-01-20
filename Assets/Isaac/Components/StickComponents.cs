078a94afa5de27f301576136f8647bf12abf27ef
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2024-01-19 23:37:22 -0500

    feet: smelly!

--- Assets/Isaac/Components/StickComponents.cs
+++ Assets/Isaac/Components/StickComponents.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class StickComponents : MonoBehaviour
 {
     // Start is called before the first frame update
-
     public List<GameObject> buildingBlocks;
     public float jointBreakForce;
     public float jointTorqueForce;
@@ -17,22 +16,43 @@ public class StickComponents : MonoBehaviour
         foreach (Transform curr in transform)
         {
             buildingBlocks.Add(curr.gameObject);
-
             foreach (Transform otherChild in transform)
             {
                 //ISSUE: double fixed joint with each other
                 if (Vector2.Distance(curr.position, otherChild.position) <= gridDistance + 0.05f
                     && otherChild != curr)
                 {
+                    //prevent overlap between fixed joints
+                    if (otherChild.GetComponent<FixedJoint2D>() != null)
+                    {
+                        if (otherChild.GetComponent<FixedJoint2D>().connectedBody == curr.GetComponent<Rigidbody2D>())
+                        {
+                            continue;
+                        }
+                    }
+
+                    //adding the fixed joint
                     FixedJoint2D f = curr.gameObject.AddComponent<FixedJoint2D>();
                     f.breakForce = jointBreakForce;
                     f.breakTorque = jointTorqueForce;
                     f.connectedBody = otherChild.gameObject.GetComponent<Rigidbody2D>();
+
+                    //ISSUE: Oscillation needs to be reduced further
+                    f.dampingRatio = 1;
+                    f.frequency = 0;
                 }
                 
             }
-
         }
-
     }
+
+    private void Update()
+    {
+        //very inefficient but, 
+        foreach (Transform curr in transform)
+        {
+            curr.GetComponent<Rigidbody2D>().angularVelocity *= 0.5f;
+        }
+    }
+
 }
